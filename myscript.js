1b13a564f18deab09085780095f933275eed96cb
Flan-231 <77691192+Flan-231@users.noreply.github.com>
2021-01-28 09:36:32 -0800

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
+	
+	$("#hide").click(function(){
+  $("p").hide();
+});
+
+$("#show").click(function(){
+  $("p").show();
+});
+	
 });
